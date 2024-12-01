b1b2e9a056674ec035806606699a16ebff1faf87
datboiikaram <zeyadseif77@gmail.com>
2024-12-01 15:21:45 +0200

    added project  links action

--- script.js
+++ script.js
@@ -2,3 +2,20 @@ const logo = document.querySelector(".mainLogo");
 logo.addEventListener("click", () => {
   alert("You're in the Home page ");
 });
+const oskarKadera = document.querySelector(".projectOne");
+const oskarKaderaLink = document.querySelector(".projectOneButton");
+oskarKadera.addEventListener("click", () => {
+  oskarKaderaLink.click();
+});
+
+const Ginseo = document.querySelector(".projectTwo");
+const GinseoLink = document.querySelector(".projectTwoButton");
+Ginseo.addEventListener("click", () => {
+  GinseoLink.click();
+});
+
+const iceCream = document.querySelector(".projectThree");
+const iceCreamLink = document.querySelector(".projectThreeButton");
+iceCream.addEventListener("click", () => {
+  iceCreamLink.click();
+});
